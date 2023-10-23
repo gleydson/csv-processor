cd3f94154ff401a0ff13b85265f46dfe8d5c282c
Gleydson Rodrigues <gleydsonsr@gmail.com>
2023-10-23 19:42:04 -0300

    chore: setup dark theme

---
+++ app/providers.tsx
@@ -0,0 +1,8 @@
+"use client"
+ 
+import { ThemeProvider } from "@/components/theme-provider"
+import { ReactNode } from "react"
+ 
+export function Providers({ children }: { children: ReactNode }) {
+  return <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>{children}</ThemeProvider>
+}
